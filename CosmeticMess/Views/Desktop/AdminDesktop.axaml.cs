da73d91fbf26aa016d1a46262a2277eaf3b7e5cc
kekmaster <kekmaster@DESKTOP-AB4C90S>
2026-04-08 15:20:48 +0300

    надо удалить из жизни админ панель

--- CosmeticMess/Views/Desktop/AdminDesktop.axaml.cs
+++ CosmeticMess/Views/Desktop/AdminDesktop.axaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using CosmeticMess.Entities;
 using WpfLikeAvaloniaNavigation;
@@ -12,26 +14,65 @@ namespace CosmeticMess.Views.Desktop;
 
 public partial class AdminDesktop : Page
 {
+    private ObservableCollection<User> _users = new();
+    private List<Role> _roles = new();
     public AdminDesktop()
     {
         InitializeComponent();
-        Load();
+        _ = Load();
     }
 
-    private async void Load()
+    private async Task Load()
     {
         var users = await API.Instance.GetUsers();
         var roles = await API.Instance.GetRoles();
 
         if (users == null || roles == null) return;
 
-        var result = new ObservableCollection<User>();
+        _roles = roles;
+        _users.Clear();
+        
         foreach (var u in users)
         {
             u.Role = roles.FirstOrDefault(r => r.Id == u.RoleId);
-            result.Add(u);
+            _users.Add(u);
         }
 
-        TableUser.ItemsSource = result;
+        TableUser.ItemsSource = _users;
+    }
+
+    private async void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.DataContext is User user)
+        {
+            var success = await API.Instance.DeleteUsers(user);
+            if (!success)
+            {
+                _users.Remove(user);
+            }
+        }
+    }
+
+    private void Back_OnClick(object? sender, RoutedEventArgs e)
+    {
+        NavigationService.Navigate(new MainDesktop());
+    }
+
+    private async void Update_OnClick(object? sender, RoutedEventArgs e)
+    {
+        await Load();
+    }
+
+    private async void Edit_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button btn && btn.DataContext is User user)
+        {
+            var parentWindow = TopLevel.GetTopLevel(this) as Window;
+            if (parentWindow == null) return;
+
+            var dialog = new EditUserWindow(user);
+            await dialog.ShowDialog(parentWindow);
+            await Load();
+        }
     }
 }
