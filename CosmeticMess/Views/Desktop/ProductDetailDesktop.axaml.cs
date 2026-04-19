4ba10c853acc0cf3dac3c82db020fae1cf8f84df
kekmaster <kekmaster@DESKTOP-AB4C90S>
2026-04-19 14:39:29 +0300

    создала доп страницу для продуктов

---
+++ CosmeticMess/Views/Desktop/ProductDetailDesktop.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using WpfLikeAvaloniaNavigation;
+
+namespace CosmeticMess.Views.Desktop;
+
+public partial class ProductDetailDesktop : Page
+{
+    public ProductDetailDesktop()
+    {
+        InitializeComponent();
+    }
+}
